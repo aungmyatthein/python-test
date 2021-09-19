ff8c50ab3453ba784dfbb8eca59ea74ab1d56ea9
Aung Myat <zinmoeleepalkmkl111@gmail.com>
2021-09-19 08:20:53 -0700

    python test

---
+++ loop.py
@@ -0,0 +1,9 @@
+names=["aung aung","thein thein","myat myat"];
+for name in names :
+    if name=='aung aung':
+        print(f'{name} is instructor')
+        break
+    else :
+        print(f'{name} is student')
+
+fruits=['apple','orange','watermelon'];
---
+++ python.py
@@ -0,0 +1,6 @@
+Username=input('Username :')
+Password=int(input('Password :'))
+if Username == "Aung Myat Thein" and Password==123456:
+    print("Log in sucess");
+else :
+    print("You are wrong");
